b5104cfe318508430f5e11e367ad7fde13afd6ad
eetum <eetum@hotmail.com>
2015-02-10 10:57:36 +0200

    Update program.js
    Added mongodb insertion. Still needs work:
    *getting data from DB (instead of getting it directly from external source.
    *updating the data in DB when necessary.

--- program.js
+++ program.js
@@ -1,3 +1,7 @@
+});
+
+});
+
 //Program.js
 //author: Eetu
 //Jan.2015
@@ -5,6 +9,11 @@
 
 
 //Reads book data from University of Helsinki and outputs it on screen.
+//and stores it on local mongoDB called booksDB.
+
+//TODO
+//server should use data from DB if available 
+//AND it is valid. If not only then fetch it from original source.
 
 var http = require('http');
 var _ = require("lodash");
@@ -14,10 +23,16 @@ var statusHtml = "<html><body>No data available!</body></html>";
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html', 'charset':'utf-8'});
-	res.end(statusHtml);
+	res.write('<!DOCTYPE "html">');
+    res.write('<meta charset="utf-8">');
+    res.end(statusHtml);
 }).listen(port, '127.0.0.1');
 
 console.log('Server running in port ' + port);
+
+
+
+
 
 var url = 'http://metadata.helmet-kirjasto.fi/search/author.json?query=Campbell';
 
@@ -41,25 +56,59 @@ http.get(url, function(res) {
         //	books.push({displayName: title, year: year});
 		
 		//new way
+        var id = 0;
 		var books = _.map(JSON.parse(body).records, function(d) {
-			return {
+			//console.log(d);
+            id++;
+            
+            return {
 				displayName: d.title,
-				year: d.year
+				year: d.year,
+                id: id
+
 			};
 		});
-		console.log(books);
-        	
+		//console.log(books);
+        
+        var jsonOutput = JSON.stringify(books);
+        //var encoded = btoa(json);
+        console.log(jsonOutput);
+
+        
+        //Connecting to MongoDB
+        var MongoClient = require('mongodb').MongoClient;
+        var dbcon = MongoClient.connect("mongodb://localhost:27017/data", function(err, db) {
+            if(!err) {
+                console.log("We are connected to DB");
+                //return db;
+            }
+
+            //Creating a collection if does not exist.
+              
+            db.createCollection('booksDB', function(err, booksDB) {});
+            //var doc1 = {'nimi': 'testinimi', 'id': '0', 'vuosi': '1900'};
+
+            var collection = db.collection('booksDB');
+                
+            //Let's insert some content
+            //TODO not inserting duplicates - either delete collection on data refresh or build more complex system.
+            collection.insert(books, {w:1}, function(err, result) {});
+        });
+
+        
         //};
         //Let's replace error HTML with content.
         statusHtml = "<html><body><h1>Kirjaluettelo</h1>";
         _.map(books, function(n) { 
-        	statusHtml += "<p>" + n.displayName + " " + n.year + "</p>"; 
+        	statusHtml += "<p>"+ n.id + " " + n.displayName + " " + n.year + "</p>"; 
         	//console.log(n.displayName, ", ", n.year);
         	
         });
         statusHtml += "</body></html>";
     	
     });
+
+
 
 
 
